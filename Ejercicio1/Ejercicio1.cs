2c1b541e05d5caaf778cac0b3b23b5386f009655
[matj1221] <matiasjcalderon@hotmail.com>
2025-05-23 20:36:15 -0300

    finalización ejercicio1

--- Ejercicio1/Ejercicio1.cs
+++ Ejercicio1/Ejercicio1.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingrese un numero entero: ");
+string? entrada = Console.ReadLine();
+
+if (int.TryParse(entrada, out int numero))
+{
+    if (numero > 0)
+    {
+        double inverso = 1.0 / numero;
+        Console.WriteLine($"El numero invertido de {numero} es: {inverso}");
+    }
+    else
+    {
+        Console.WriteLine("No se puede dividir en 0.");
+    }
+}
+else
+{
+    Console.WriteLine("NO se ingreso un numero entero.");
+}
+

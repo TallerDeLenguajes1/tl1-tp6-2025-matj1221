f96bf4d6ddc6b037abb25447aa39c7d7df606db9
[matj1221] <matiasjcalderon@hotmail.com>
2025-05-23 20:47:27 -0300

    guardado calculadoraV2

--- Ejercicio2/Ejercicio2.cs
+++ Ejercicio2/Ejercicio2.cs
@@ -129,3 +129,59 @@ int Dividir(int a, int b)
 {
     return a / b;
 }
+
+//Calculadora V2
+Console.WriteLine("Ingrese un numero real: ");
+string? entrada3 = Console.ReadLine();
+double numero3;
+if (double.TryParse(entrada3, out numero3))
+{
+    if (numero3 >= 0)
+    {
+        Console.WriteLine($"Valor absoluto de {numero3}: {numero3}");
+    }
+    else
+    {
+        Console.WriteLine($"Valor absoluto de {numero3}: {-numero3}");
+    }
+
+    Console.WriteLine($"El cuadrado de {numero3}: {numero3 * numero3}");
+    Console.WriteLine($"La raiz cuadrada de {numero3}: {Math.Sqrt(numero3)}");
+    Console.WriteLine($"El seno de {numero3}: {Math.Sin(numero3 * Math.PI / 180)}");
+    Console.WriteLine($"El coseno de {numero3}: {Math.Cos(numero3 * Math.PI / 180)}");
+    Console.WriteLine($"La parte entera de {numero3}: {Math.Truncate(numero3)}");
+}
+
+
+string? entrada4, entrada5;
+double numero4, numero5;
+
+Console.WriteLine("Ingrese un numero real: ");
+entrada4 = Console.ReadLine();
+if (double.TryParse(entrada4, out numero4))
+{
+    Console.WriteLine("Ingrese otro numero real: ");
+    entrada5 = Console.ReadLine();
+    if (double.TryParse(entrada5, out numero5))
+    {
+        if (numero4 > numero5)
+        {
+            Console.WriteLine($"El mayor es: {numero4}");
+            Console.WriteLine($"El menor es: {numero5}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es: {numero5}");
+            Console.WriteLine($"El menor es: {numero4}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("No ingresaste un numero real. ");
+
+    }
+}
+else
+{
+    Console.WriteLine("No ingresaste un numero real. ");
+}
